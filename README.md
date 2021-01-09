3bbf3bcfb1e53d71608b5082e090fe89b19c63de
iagokrt <iago_floripa@hotmail.com>
2021-01-09 02:10:36 -0300

    initial commit for turtles with create react app

---
+++ README.md
@@ -0,0 +1,31 @@
+# NightEdges
+
+## _January Days_
+
+### Package manager
+
+- [x] yarn
+
+### deploy
+
+- [x] netlify
+
+### frameworks
+
+- [x] reactjs CRA
+- [ ] three core [**https://threejs.org/docs/**](https://threejs.org/docs/)
+- [ ] react three fiber renderer
+
+for stylings
+
+- [ ] linaria
+
+for linting
+
+- [ ] eslint
+- [ ] prettier
+- [ ] editorconfig
+
+### postprocessing r3f
+
+- [ ] drei
---
+++ src/App.js
@@ -0,0 +1,22 @@
+import logo from './logo.svg'
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>Coding traffic, Artistic Bundle</p>
+        <a
+          className="App-link"
+          href="https://writeisland.netlify.app/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          WriteIsland v1
+        </a>
+      </header>
+    </div>
+  )
+}
+
+export default App
